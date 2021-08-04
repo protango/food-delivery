f10d49fc423c5ae86ca4e7a730c3d6fe85c5e7f2
Zac Murray <zacharymurray@hotmail.com>
2021-08-04 21:21:12 +1000

    Import bootstrap into vue app

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -4,4 +4,9 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/dist/jquery.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
 createApp(App).use(store).use(router).mount('#app')
