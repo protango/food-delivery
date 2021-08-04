eb5e30faf192d5919406ba2e6140dc534f831015
Zac Murray <zacharymurray@hotmail.com>
2021-08-04 18:10:47 +1000

    Db Model initial

---
+++ Db/Model.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFGetStarted
+{
+    public class FoodDeliveryContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+        public FoodDeliveryContext()
+        {
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options) {
+            options.UseNpgsql("Host=localhost;Database=my_db;Username=my_user;Password=my_pw");
+        }
+    }
+
+    public class Restaurant
+    {
+        public long Id { get; set; }
+    }
+}
