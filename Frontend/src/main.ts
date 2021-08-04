35b635c7ed6f5aab177e97a9c5734c9bf9a483e7
Zac Murray <zacharymurray@hotmail.com>
2021-08-04 21:54:14 +1000

    Put semicolons back

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,11 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
 
 // Bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'jquery/dist/jquery.min.js'
-import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap';
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount('#app');
