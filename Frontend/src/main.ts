265698cf7e063baeb0aed57fb575c1614820320a
Zac Murray <zacharymurray@hotmail.com>
2021-08-04 21:30:44 +1000

    Remove vuex

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -2,11 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-import store from './store'
 
 // Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'jquery/dist/jquery.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(router).mount('#app')
