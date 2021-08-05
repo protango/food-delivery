cd30bdae281a42f911c0261df6a77f95aee7c054
Zac Murray <zacharymurray@hotmail.com>
2021-08-05 14:51:28 +1000

    Generate DB Migration

--- Migrations/20210805044923_InitialCreate.Designer.cs
+++ Migrations/20210805044923_InitialCreate.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using FoodDelivery.Db;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace FoodDelivery.Migrations
 {
     [DbContext(typeof(FoodDeliveryContext))]
-    partial class FoodDeliveryContextModelSnapshot : ModelSnapshot
+    [Migration("20210805044923_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
