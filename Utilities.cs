fba041e01156f0fe37ed1c24347f921c7c44640a
Zac Murray <zacharymurray@hotmail.com>
2021-08-06 12:30:20 +1000

    Add utility function to extract user id

---
+++ Utilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace FoodDelivery
+{
+    public static class Utilities
+    {
+        public static string ExtractUserId(ClaimsPrincipal user) {
+            var idClaim = user.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+            if (idClaim == null)
+                throw new Exception("ClaimsPrincipal does not contain a NameIdentifier claim");
+            return idClaim.Value;
+        }
+    }
+}
