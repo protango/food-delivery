b2e8e57b1c081096793f719d61fb850ca594fcac
Zac Murray <zacharymurray@hotmail.com>
2021-08-07 18:33:00 +1000

    Add place holder meal editor

---
+++ Frontend/src/components/MealEditor.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Placeholder meal editor for {{restaurantId}}</div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    restaurantId: Number
+  }
+})
+export default class MealEditor extends Vue {
+  public restaurantId?: number;
+}
+</script>
+
+<style scoped lang="scss">
+</style>
