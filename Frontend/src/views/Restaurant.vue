fa8550d46bc86ad3002a7b9e3e07e6db85b1086a
Zac Murray <zacharymurray@hotmail.com>
2021-08-07 22:40:10 +1000

    Added rendering core to restaurant page

--- Frontend/src/views/Restaurant.vue
+++ Frontend/src/views/Restaurant.vue
@@ -1,11 +1,22 @@
 <template>
   <loading-overlay v-if="!restaurant" message="Loading"></loading-overlay>
   <div v-if="restaurant">
-    <h1>{{restaurant.name}}</h1>
-    <h2>{{restaurant.description}}</h2>
-    <table class="table">
-
-    </table>
+    <h1>Viewing Restaurant</h1>
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">{{restaurant.name}}</h5>
+        <p class="card-text">{{restaurant.description}}</p>
+        <router-link :to="`/dashboard/edit-restaurant/${restaurant.id}`" class="btn btn-primary me-1">Edit restaurant</router-link>
+        <a class="btn btn-danger" @click="deleteRestaurant">Delete restaurant</a>
+      </div>
+      <div class="card-header border-top">
+        Orders for this restaurant
+      </div>
+      <ul class="list-group list-group-flush">
+        <li v-if="!orders.length" class="list-group-item text-muted">No orders yet</li>
+        <li v-for="order in orders" :key="order.id" class="list-group-item">{{order.id}}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -16,6 +27,7 @@
 <script lang="ts">
 import { Vue, Options } from 'vue-class-component';
 import { Restaurant, RestaurantService } from '../services/restaurantService';
+import { Order, OrderService } from '../services/orderService';
 import LoadingOverlay from '../components/LoadingOverlay.vue';
 @Options({
   components: {
@@ -24,7 +36,14 @@ import LoadingOverlay from '../components/LoadingOverlay.vue';
 })
 export default class Restaurants extends Vue {
   public restaurant: Restaurant | null = null;
+  public orders: Order[] = [];
   public restaurantId!: number;
+
+  public async deleteRestaurant (): Promise<void> {
+    this.restaurant = null;
+    await RestaurantService.delete(this.restaurantId);
+    this.$router.push('/dashboard/restaurants');
+  }
 
   public async beforeCreate (): Promise<void> {
     try {
@@ -34,6 +53,7 @@ export default class Restaurants extends Vue {
       }
 
       this.restaurant = await RestaurantService.get(this.restaurantId);
+      this.orders = await OrderService.getForRestaurant(this.restaurantId);
     } catch {
       this.$router.push('/');
     }
