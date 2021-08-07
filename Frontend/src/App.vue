4cc9d889acd87509bee63bf3e51aec8977940367
Zac Murray <zacharymurray@hotmail.com>
2021-08-07 17:20:52 +1000

    h1 style change

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -7,4 +7,9 @@ $fa-font-path: '~@fortawesome/fontawesome-free/webfonts';
 @import '~@fortawesome/fontawesome-free/scss/fontawesome';
 @import '~@fortawesome/fontawesome-free/scss/solid';
 @import '~@fortawesome/fontawesome-free/scss/regular';
+
+h1 {
+  margin-top: 10px !important;
+  margin-bottom: 20px !important;
+}
 </style>
