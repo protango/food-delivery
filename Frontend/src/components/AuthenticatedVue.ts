6e2603d4bf0de2588c08abee6ab261bbeec62dfd
Zac Murray <zacharymurray@hotmail.com>
2021-08-08 13:38:41 +1000

    Auth redirect fix

--- Frontend/src/components/AuthenticatedVue.ts
+++ Frontend/src/components/AuthenticatedVue.ts
@@ -1,9 +1,20 @@
-import { AuthService } from '@/services/authService';
+import { AuthService, UserData } from '@/services/authService';
 import { Vue } from 'vue-class-component';
 
+function getLoggedInUser (): UserData {
+  if (AuthService.loggedInUser) return AuthService.loggedInUser;
+  return {
+    id: '',
+    expiry: new Date(),
+    isExpired: true,
+    roles: [],
+    token: '',
+    username: 'UNAUTHENTICATED'
+  };
+}
 export default class AuthenticatedVue extends Vue {
-    public isCustomer: boolean = AuthService.loggedInUser!.roles.includes('CUSTOMER');
-    public isOwner: boolean = AuthService.loggedInUser!.roles.includes('RESTAURANT_OWNER');
-    public username: string = AuthService.loggedInUser!.username;
-    public userId: string = AuthService.loggedInUser!.id;
+    public isCustomer: boolean = getLoggedInUser().roles.includes('CUSTOMER');
+    public isOwner: boolean = getLoggedInUser().roles.includes('RESTAURANT_OWNER');
+    public username: string = getLoggedInUser().username;
+    public userId: string = getLoggedInUser().id;
 }
